a5c8bcf64a69e07810e71e5114bb3cc8ea45c1f6
katalyn4me <katalyn4me@yahoo.com>
2018-12-19 10:24:58 +0100

    remove str_replace in xml response

--- PSWebServiceLibrary.php
+++ PSWebServiceLibrary.php
@@ -201,7 +201,6 @@ class PrestaShopWebservice
 		{
 			libxml_clear_errors();
 			libxml_use_internal_errors(true);
-            $response = str_replace(array("\r\n", "\r", "\n","[]"), "", $response);
 			$xml = simplexml_load_string(trim($response),'SimpleXMLElement', LIBXML_NOCDATA);
 			if (libxml_get_errors())
 			{
